5b431c24f20fb8c0441aea8067b95536a9b5614d
Jerremy Stroman <jerremystroman@gmail.com>
2022-04-19 11:52:03 -0700

    Basic structure setup

---
+++ server/routes/products.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+
+const router = express.Router();
+
+console.log('products API running');
+
+// Code goes here
+
+module.exports = router;
