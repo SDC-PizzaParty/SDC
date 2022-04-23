2c7984554d802a81f4ccf7aa86f43130c319c7a9
Jerremy Stroman <jerremystroman@gmail.com>
2022-04-23 13:15:50 -0700

    Adding tests

--- tests/product.test.js
+++ tests/product.test.js
@@ -6,30 +6,42 @@ const API_URL = 'http://127.0.0.1:3666';
 const API_TEST_URL = 'http://127.0.0.1:3666/test';
 const db = new Pool({ database: process.env.PRODUCT_DB });
 
-test('Receives a response from the product API', () => {
-  // const sum = (a, b) => a + b;
-  // expect(sum(1, 2)).toBe(3);
-  request(`${API_URL}/`)
-    .then((response) => {
-      expect(response).toEqual('Response from Product API');
-    });
+describe('Product API response', () => {
+  test('Receives a response from the product API', () => {
+    // const sum = (a, b) => a + b;
+    // expect(sum(1, 2)).toBe(3);
+    request(`${API_URL}/`)
+      .then((response) => {
+        expect(response).toEqual('Response from Product API');
+      });
+  });
 });
 
-describe('Product API data retrieval:', () => {
+describe('Product API data retrieval [PRODUCTS]:', () => {
   const testProductId = 5;
 
-  request(`${API_TEST_URL}/${testProductId}`)
-    .then((response) => {
-      const parsedResponse = JSON.parse(response);
+  test('Receives a Javascript object from the server', () => {
+    request(`${API_TEST_URL}/${testProductId}`)
+      .then((response) => {
+        const parsedResponse = JSON.parse(response);
+        expect(parsedResponse).toBeInstanceOf(Object);
+      });
+  });
 
-      const queryString = 'SELECT * FROM product WHERE id = $1';
-      const queryValues = [testProductId];
+  test('Receives the item from the database given its product ID', () => {
+    request(`${API_TEST_URL}/${testProductId}`)
+      .then((response) => {
+        const parsedResponse = JSON.parse(response);
+        const queryString = 'SELECT * FROM product WHERE id = $1';
+        const queryValues = [testProductId];
 
-      db.query(queryString, queryValues)
-        .then((result) => {
-          it('Receives the item from the database given its product ID', () => {
+        // Matchers to explore:
+        // toMatchObject -> match the general 'gist' of an object
+
+        db.query(queryString, queryValues)
+          .then((result) => {
             expect(parsedResponse).toBe(result.rows[0]);
           });
-        });
-    });
+      });
+  });
 });
